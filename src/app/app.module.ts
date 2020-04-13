9b16b50229a348abe8aab1a6a2eff304eb7e88ea
Niggarilomon <losgamers123456@gmail.com>
2020-04-12 22:37:09 -0300

    markers showing in the map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { HTTP } from '@ionic-native/http/ngx';
 import { GoogleMaps } from '@ionic-native/google-maps/ngx';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
-import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -29,7 +28,6 @@ import { AppRoutingModule } from './app-routing.module';
     GoogleMaps,
     HTTP,
     Geolocation,
-    NativeGeocoder
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,19 @@
 import {
   GoogleMaps,
   GoogleMap,
-  GoogleMapsEvent,
   GoogleMapOptions,
   Circle,
+  Marker,
   Environment,
-  ILatLng
+  HtmlInfoWindow,
+  GoogleMapsEvent
 } from '@ionic-native/google-maps/ngx';
 import { Component } from "@angular/core";
 import { Platform } from '@ionic/angular';
 
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { HTTP } from '@ionic-native/http/ngx';
 import { Storage } from '@ionic/storage';
-import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
 
 
 @Component({
@@ -22,17 +23,17 @@ import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@io
 })
 export class HomePage {
   map: GoogleMap;
+  actualNumber: 0;
 
   constructor(
     public storage: Storage,
     private platform: Platform,
     private http: HTTP,
-    private nativeGeocoder: NativeGeocoder) { }
+    private geolocation: Geolocation) { }
 
   ngOnInit() {
     this.platform.ready();
     this.loadMap();
-    this.getCircles();
   }
 
   loadMap() {
@@ -41,49 +42,124 @@ export class HomePage {
       'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyCOwpKWB6VSvclPt6yoUJIP_jk9LVvzOsM'
     });
 
-    let mapOptions: GoogleMapOptions = {
-      camera: {
-         target: {
-           lat: 43.0741904,
-           lng: -89.3809802
-         },
-         zoom: 18,
-         tilt: 30
-       },
-       controls: {
-        'compass': true,
-        'myLocationButton': true,
-        'myLocation': true, 
-        'indoorPicker': true,
-        'zoom': false,       
-        'mapToolbar': true  
-      }
-    };
 
-    this.map = GoogleMaps.create('map_canvas', mapOptions);
+    this.geolocation.getCurrentPosition().then((resp) => {
+      let mapOptions: GoogleMapOptions = {
+        camera: {
+           target: {
+             lat: resp.coords.latitude,
+             lng: resp.coords.longitude
+           },
+           zoom: 18,
+           tilt: 30
+         },
+         controls: {
+          'compass': true,
+          'myLocationButton': false,
+          'myLocation': true, 
+          'indoorPicker': true,
+          'zoom': false,       
+          'mapToolbar': true  
+        }
+      };
+  
+      this.map = GoogleMaps.create('map_canvas', mapOptions);
+      this.map.on(GoogleMapsEvent.CAMERA_MOVE_END).subscribe(() => {
+        const zoom = this.map.getCameraZoom(); 
+        if (zoom < 10 && zoom > 6)
+          this.insertControll(1);
+        else if (zoom < 15 && zoom > 10)
+          this.insertControll(2);
+      })
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+    
     
   }
 
-  getCircles() {
+  async insertControll(number) {
+    const date = new Date();
+
+    if (number == 1 && number != this.actualNumber) {
+      this.actualNumber = number;
+      const data = await this.storage.get('allStates').then(val => val);
+      if (data == null || 
+        (date.getDate() > data.date.day ||
+        date.getMonth() > data.date.month ||
+        date.getFullYear() > data.date.year))
+          this.getAllStates();
+
+      this.drawCircles(data.cleanData);
+    }
+    else if (number == 2 && number != this.actualNumber) {
+      this.actualNumber = number;
+      const data = await this.storage.get('perState').then(val => val);
+      console.log(data)
+      if (data == null || 
+        (date.getDate() > data.date.day ||
+        date.getMonth() > data.date.month ||
+        date.getFullYear() > data.date.year))
+          this.getPerState();
+
+      this.drawCircles(data.cleanData);
+    }
+  }
+
+  getAllStates() {
+    const d = new Date()
+    console.log(d.getDate());
     this.storage.get('token').then(value => {
       this.http.get('https://coronago.herokuapp.com/coronaApi/getAllStates', {}, {
         'Authorization': `Bearrer ${value}`
       }).then(data => {
+        const date = new Date();
         const { cleanData } = JSON.parse(data.data);
-
-        let options: NativeGeocoderOptions = {
-          useLocale: true,
-          maxResults: 5
-        };
-        
-        this.nativeGeocoder.forwardGeocode('Alagoas', options)
-          .then((result: NativeGeocoderResult[]) => {
-            console.log('The coordinates are latitude=' + result[0].latitude + ' and longitude=' + result[0].longitude)
-          })
-          .catch((error: any) => console.log(error));
+          this.storage.set('allStates', { 
+            cleanData,
+            date: { year: date.getFullYear(), month: date.getMonth(), day: date.getDate() } });
+          this.drawCircles(cleanData);
+        });
       });
-    });
+  }
 
-  
+  getPerState() {
+    const d = new Date()
+    console.log(d.getDate());
+    this.storage.get('token').then(value => {
+      this.http.get('https://coronago.herokuapp.com/coronaApi/getPerState/*', {}, {
+        'Authorization': `Bearrer ${value}`
+      }).then(data => {
+        const date = new Date();
+        const { cleanData } = JSON.parse(data.data);
+          this.storage.set('perState', { 
+            cleanData,
+            date: { year: date.getFullYear(), month: date.getMonth(), day: date.getDate() } });
+          this.drawCircles(cleanData);
+        });
+      });
+  }
+
+  drawCircles(array) {
+    this.map.clear();
+    array.forEach(element => {
+      const { latitude, longitude } = element.position;
+
+      let marker: Marker = this.map.addMarkerSync({
+        position: { lat: latitude, lng: longitude },
+        title: element.state,
+        icon: 'blue',
+        animation: 'DROP',
+      });
+      
+      //console.log(element.confirmed * (this.map.getCameraZoom() / 0.1))
+      let circle: Circle = this.map.addCircleSync({
+        center: marker.getPosition(),
+        radius: element.confirmed * 30,
+        fillColor: "rgba(255, 0, 0, 0.5)",
+      });
+      marker.bindTo("position", circle, "center");
+      
+    });
   }
 }
